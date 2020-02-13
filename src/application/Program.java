234d17f09ce32fe0bec124fbe3619276b6d05080
Gabriel Silvestre <gabrielsilvestre3993@gmail.com>
2020-02-13 20:47:35 -0300

    apaguei boa tarde e noite

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,8 +5,6 @@ public class Program {
 	public static void main(String[] args) {
 		
 		System.out.println("BOM DIA!!");
-		System.out.println("Boa tarde!!");
-		System.out.println("Boa noite!!");
 		
 	}
 
